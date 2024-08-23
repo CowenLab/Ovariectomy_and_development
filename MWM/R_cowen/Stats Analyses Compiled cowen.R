b89aa0490dbac8287c6fc016fe171991ed2abd72
Stephen Cowen <scowen@email.arizona.edu>
2024-08-23 14:52:33 -0700

    Update Stats Analyses Compiled cowen.R

--- MWM/R_cowen/Stats Analyses Compiled cowen.R
+++ MWM/R_cowen/Stats Analyses Compiled cowen.R
@@ -38,8 +38,8 @@ TABLExF_Full$trial_num[TABLExF_Full$trial_num > 31 & TABLExF_Full$trial_num <= 3
 
 # Eliminate the probe trials from analysis as we are not using them as we don't have enough data for now.
 # 
-MWM <- subset(TABLExF_Full, trial_num < 7)
-
+ MWM <- subset(TABLExF_Full, trial_num < 7)
+#MWM <- subset(TABLExF_Full, trial_num > 3 & trial_num < 7)
 
 MWM$is_thigmotaxis = (MWM$strategy == 1)*1
 MWM$is_circling    = (MWM$strategy == 2)*1
@@ -51,33 +51,51 @@ MWM$is_corrected_path  = (MWM$strategy == 7)*1
 MWM$is_direct_path     = (MWM$strategy == 8)*1 
 MWM$is_perseverance    = (MWM$strategy == 9)*1
 
-TB$perf = TB$mn_dir_path + TB$mn_cor_path
+TB$perf = TB$mn_dir_path + TB$mn_corrected
 
 ggplot(data = MWM, aes(x = trial_num, CIPL_Scores, color = Strain)) + geom_point() + facet_wrap(~day_cat)
 
-
 hist( MWM$strategy )
-
-#MWM[MWM$strategy == 8,"strategy_cat"]
 
 mean(MWM$is_thigmotaxis,na.rm = T)
 mean(MWM$is_directed_search,na.rm = T)
 
-TB <- MWM %>% group_by(day_cat, Strain, animalID, Age.months.) %>% summarize(mn_thig = mean(is_thigmotaxis), mn_circ = mean(is_circling) , mn_rnd = mean(is_random_path) , mn_scan = mean(is_scanning) , mn_chain = mean(is_chaining) , mn_direct = mean(is_directed_search) , mn_cor_path = mean(is_corrected_path ), mn_dir_path = mean(is_direct_path), mn_persev = mean(is_perseverance) )
+TB <- MWM %>% group_by(day_cat, Strain, animalID, Age.months.) %>% summarize(mn_thig = mean(is_thigmotaxis), mn_circ = mean(is_circling) , mn_rnd = mean(is_random_path) , mn_scan = mean(is_scanning) , mn_chain = mean(is_chaining) , mn_direct = mean(is_directed_search) , mn_corrected = mean(is_corrected_path ), mn_dir_path = mean(is_direct_path), mn_persev = mean(is_perseverance), mn_CIPL = mean(CIPL_Scores) )
 
+# Strategies from the Konsolaki 2016 paper.
 TB$escape = TB$mn_thig + TB$mn_rnd
-TB$global = TB$mn_scan + TB$mn_chain
-TB$local = TB$mn_dir_path + TB$mn_cor_path
+TB$local = TB$mn_scan + TB$mn_chain
+TB$global = TB$mn_dir_path + TB$mn_corrected # Directed search is not included.
+# Other metrics.
+TB$perf = TB$global-TB$escape
+TB$allocentric = TB$mn_dir_path + TB$mn_corrected + TB$mn_direct
 
-plot(TB$mn_thig)
-plot(TB$mn_dir_path )
-plot(TB$mn_dir_path + TB$mn_cor_path )
-ggplot(data = TB, aes(x = day_cat, perf, color = Strain)) + geom_violin() + geom_point(position=position_jitterdodge(dodge.width=0.9))  + facet_wrap(~Age.months.)
-ggplot(data = TB, aes(x = day_cat, perf, color = Strain)) + geom_boxplot() + geom_point(position=position_jitterdodge(dodge.width=0.9))  + facet_wrap(~Age.months.)
-ggplot(data = TB, aes(x = Age.months., perf, color = Strain)) + geom_boxplot() + geom_point(position=position_jitterdodge(dodge.width=0.009))  + facet_wrap(~day_cat)
+# Look at the effect of DAY on performance treating each Strain (OVX vs. Sham)
+ggplot(data = TB, aes(x = day_cat, y = escape, color = Strain)) + geom_violin() + geom_point(position=position_jitterdodge(dodge.width=0.9))  + facet_wrap(~Age.months.)
+ggplot(data = TB, aes(x = day_cat, y = local, color = Strain)) + geom_violin() + geom_point(position=position_jitterdodge(dodge.width=0.9))  + facet_wrap(~Age.months.)
+ggplot(data = TB, aes(x = day_cat, y = global, color = Strain)) + geom_violin() + geom_point(position=position_jitterdodge(dodge.width=0.9))  + facet_wrap(~Age.months.)
+ggplot(data = TB, aes(x = day_cat, y = perf, color = Strain)) + geom_violin() + geom_point(position=position_jitterdodge(dodge.width=0.9))  + facet_wrap(~Age.months.)
+ggplot(data = TB, aes(x = day_cat, y = allocentric, color = Strain)) + geom_violin() + geom_point(position=position_jitterdodge(dodge.width=0.9))  + facet_wrap(~Age.months.)
+ggplot(data = TB, aes(x = day_cat, y = mn_CIPL, color = Strain)) + geom_violin() + geom_point(position=position_jitterdodge(dodge.width=0.9))  + facet_wrap(~Age.months.)
 
-ggplot(data = TB, aes(x = trial_num, perf, color = Strain)) + geom_point() + facet_wrap(~day_cat)
+# What correlates the best with CIPL?
+ggplot(data = TB, aes(x =perf, y = mn_CIPL, color = Strain)) + geom_point()+ facet_wrap(~Age.months.) +  geom_smooth()
+ggplot(data = TB, aes(x =allocentric, y = mn_CIPL, color = Strain)) + geom_point()+ facet_wrap(~Age.months.) +  geom_smooth()
+ggplot(data = TB, aes(x =escape, y = mn_CIPL, color = Strain)) + geom_point()+ facet_wrap(~Age.months.) +  geom_smooth()
 
+# Is there a general effect of age?
+ggplot(data = TB, aes(x = factor(Age.months.), y = mn_CIPL, color = Strain)) + geom_violin() + geom_point(position=position_jitterdodge(dodge.width=0.9))  + facet_wrap(~day_cat)
+ggplot(data = TB, aes(x = factor(Age.months.), y = perf, color = Strain)) + geom_violin() + geom_point(position=position_jitterdodge(dodge.width=0.9))  + facet_wrap(~day_cat)
+# Same question but use a regression/scatter plot. Should be a correlation.
+ggplot(data = TB, aes(x =Age.months., y = mn_CIPL, color = Strain)) + geom_point()+ facet_wrap(~day_cat) +  geom_smooth()
+
+
+# ###################
+
+ggplot(data = TB, aes(x = day_cat, global, color = Strain)) + geom_boxplot() + geom_point(position=position_jitterdodge(dodge.width=0.9))  + facet_wrap(~Age.months.)
+ggplot(data = TB, aes(x = Age.months., global, color = Strain)) + geom_boxplot() + geom_jitter() + facet_wrap(~day_cat)
+
+ggplot(data = TB, aes(x = Age.months., global, color = Strain)) + geom_jitter() + facet_wrap(~day_cat)
 
 
 boxplot(CIPL_Scores ~ Strain * name, data = MWM)
@@ -86,168 +104,3 @@ boxplot(is_thigmotaxis ~ Strain * name, data = MWM)
 
 AnovaCIPLaov2_xF_Full <- aov(CIPL_Scores ~ Strain * X_Day, data = MWM)
 EtaCIPL_xF_Full <- etaSquared(AnovaCIPLaov2_xF_Full)
-
-AnovaCIPLaov2_xF_Full <- aov(CIPL_Scores ~ Strain * X_Day * Age.months, data = MWM)
-
-
-MWM$Age.months <- as.factor(MWM$Age.months)
-AnovaCIPLaov2_xF_Full_Age <- aov(CIPL_Scores ~ Strain * Age.months, data = MWM)
-tukey <- TukeyHSD(AnovaCIPLaov2_xF_Full_Age, "Age.months")
-print(tukey)
-
-boxplot(CIPL_Scores ~ Strain * strategy, data = MWM)
-
-
-MWM <- MWM %>%
-  rename(Age.months = Age.months.)
-
-#NEW - 2 month old cohort
-TABLExA_2m <- read.csv("C:/Users/Moreau/Desktop/WaterMazeAnalysis/R Files/Results/2 Months - Full/2MCombined_StrategiesResults_xA.csv")
-
-TABLExR_2m <- read.csv("C:/Users/Moreau/Desktop/WaterMazeAnalysis/R Files/Results/2 Months - Full/2MCombined_StrategiesResults_xR.csv")
-
-AnovaCIPLaov2_xA_2m <- aov(CIPL_Scores ~ Strain * X_Day, data = TABLExA_2m)
-EtaCIPL_xA_2m <- etaSquared(AnovaCIPLaov2_xA_2m)
-AnovaStrataov2_xA_2m <- aov(strategy ~ Strain * X_Day, data = TABLExA_2m)
-EtaStrat_xA_2m <- etaSquared(AnovaStrataov2_xA_2m)
-summary(AnovaCIPLaov2_xA_2m)
-print(EtaCIPL_xA_2m)
-summary(AnovaStrataov2_xA_2m)
-print(EtaStrat_xA_2m)
-
-
-AnovaCIPLaov2_xR_2m <- aov(CIPL_Scores ~ Strain * X_Day, data = TABLExR_2m)
-EtaCIPL_xR_2m <- etaSquared(AnovaCIPLaov2_xR_2m)
-AnovaStrataov2_xR_2m <- aov(strategy ~ Strain * X_Day, data = TABLExR_2m)
-EtaStrat_xR_2m <- etaSquared(AnovaStrataov2_xR_2m)
-summary(AnovaCIPLaov2_xR_2m)
-print(EtaCIPL_xR_2m)
-summary(AnovaStrataov2_xR_2m)
-print(EtaStrat_xA_2m)
-
-
-Means_xA_2m <- aggregate(TABLExA_2m$CIPL_Scores, by = list(TABLExA_2m$Strain), FUN = mean)
-print(Means_xA_2m)
-Means_xR_2m <- aggregate(TABLExR_2m$CIPL_Scores, by = list(TABLExR_2m$Strain), FUN = mean)
-print(Means_xR_2m)
-
-MeansStrat_xA_2m <- aggregate(TABLExA_2m$strategy, by = list(TABLExA_2m$Strain), FUN = mean)
-print(MeansStrat_xA_2m)
-MeansStrat_xR_2m <- aggregate(TABLExR_2m$strategy, by = list(TABLExR_2m$Strain), FUN = mean)
-print(MeansStrat_xR_2m)
-
-
-#NEW - 6 month old cohort
-TABLExA_6m <- read.csv("C:/Users/Moreau/Desktop/WaterMazeAnalysis/R Files/Results/6 Months - Full/6MCombined_StrategiesResults_xA.csv")
-
-TABLExR_6m <- read.csv("C:/Users/Moreau/Desktop/WaterMazeAnalysis/R Files/Results/6 Months - Full/6MCombined_StrategiesResults_xR.csv")
-
-AnovaCIPLaov2_xA_6m <- aov(CIPL_Scores ~ Strain * X_Day, data = TABLExA_6m)
-EtaCIPL_xA_6m <- etaSquared(AnovaCIPLaov2_xA_6m)
-AnovaStrataov2_xA_6m <- aov(strategy ~ Strain * X_Day, data = TABLExA_6m)
-EtaStrat_xA_6m <- etaSquared(AnovaStrataov2_xA_6m)
-summary(AnovaCIPLaov2_xA_6m)
-print(EtaCIPL_xA_6m)
-summary(AnovaStrataov2_xA_6m)
-print(EtaStrat_xA_6m)
-
-
-AnovaCIPLaov2_xR_6m <- aov(CIPL_Scores ~ Strain * X_Day, data = TABLExR_6m)
-EtaCIPL_xR_6m <- etaSquared(AnovaCIPLaov2_xR_6m)
-AnovaStrataov2_xR_6m <- aov(strategy ~ Strain * X_Day, data = TABLExR_6m)
-EtaStrat_xR_6m <- etaSquared(AnovaStrataov2_xR_6m)
-summary(AnovaCIPLaov2_xR_6m)
-print(EtaCIPL_xR_6m)
-summary(AnovaStrataov2_xR_6m)
-print(EtaStrat_xA_6m)
-
-
-Means_xA_6m <- aggregate(TABLExA_6m$CIPL_Scores, by = list(TABLExA_6m$Strain), FUN = mean)
-print(Means_xA_6m)
-Means_xR_6m <- aggregate(TABLExR_6m$CIPL_Scores, by = list(TABLExR_6m$Strain), FUN = mean)
-print(Means_xR_6m)
-
-MeansStrat_xA_6m <- aggregate(TABLExA_6m$strategy, by = list(TABLExA_6m$Strain), FUN = mean)
-print(MeansStrat_xA_6m)
-MeansStrat_xR_6m <- aggregate(TABLExR_6m$strategy, by = list(TABLExR_6m$Strain), FUN = mean)
-print(MeansStrat_xR_6m)
-
-
-
-
-#NEW - 9 month old cohort
-TABLExA_9m <- read.csv("C:/Users/Moreau/Desktop/WaterMazeAnalysis/R Files/Results/9 Months - Full/9MCombined_StrategiesResults_xA.csv")
-
-TABLExR_9m <- read.csv("C:/Users/Moreau/Desktop/WaterMazeAnalysis/R Files/Results/9 Months - Full/9MCombined_StrategiesResults_xR.csv")
-
-AnovaCIPLaov2_xA_9m <- aov(CIPL_Scores ~ Strain * X_Day, data = TABLExA_9m)
-EtaCIPL_xA_9m <- etaSquared(AnovaCIPLaov2_xA_9m)
-AnovaStrataov2_xA_9m <- aov(strategy ~ Strain * X_Day, data = TABLExA_9m)
-EtaStrat_xA_9m <- etaSquared(AnovaStrataov2_xA_9m)
-summary(AnovaCIPLaov2_xA_9m)
-print(EtaCIPL_xA_9m)
-summary(AnovaStrataov2_xA_9m)
-print(EtaStrat_xA_9m)
-
-
-AnovaCIPLaov2_xR_9m <- aov(CIPL_Scores ~ Strain * X_Day, data = TABLExR_9m)
-EtaCIPL_xR_9m <- etaSquared(AnovaCIPLaov2_xR_9m)
-AnovaStrataov2_xR_9m <- aov(strategy ~ Strain * X_Day, data = TABLExR_9m)
-EtaStrat_xR_9m <- etaSquared(AnovaStrataov2_xR_9m)
-summary(AnovaCIPLaov2_xR_9m)
-print(EtaCIPL_xR_9m)
-summary(AnovaStrataov2_xR_9m)
-print(EtaStrat_xA_9m)
-
-
-Means_xA_9m <- aggregate(TABLExA_9m$CIPL_Scores, by = list(TABLExA_9m$Strain), FUN = mean)
-print(Means_xA_9m)
-Means_xR_9m <- aggregate(TABLExR_9m$CIPL_Scores, by = list(TABLExR_9m$Strain), FUN = mean)
-print(Means_xR_9m)
-
-MeansStrat_xA_9m <- aggregate(TABLExA_9m$strategy, by = list(TABLExA_9m$Strain), FUN = mean)
-print(MeansStrat_xA_9m)
-MeansStrat_xR_9m <- aggregate(TABLExR_9m$strategy, by = list(TABLExR_9m$Strain), FUN = mean)
-print(MeansStrat_xR_9m)
-
-
-
-
-#NEW - 14 month old cohort
-TABLExA_14m <- read.csv("C:/Users/Moreau/Desktop/WaterMazeAnalysis/R Files/Results/14 Months - Full/14MCombined_StrategiesResults_xA.csv")
-
-TABLExR_14m <- read.csv("C:/Users/Moreau/Desktop/WaterMazeAnalysis/R Files/Results/14 Months - Full/14MCombined_StrategiesResults_xR.csv")
-
-AnovaCIPLaov2_xA_14m <- aov(CIPL_Scores ~ Strain * X_Day, data = TABLExA_14m)
-EtaCIPL_xA_14m <- etaSquared(AnovaCIPLaov2_xA_14m)
-AnovaStrataov2_xA_14m <- aov(strategy ~ Strain * X_Day, data = TABLExA_14m)
-EtaStrat_xA_14m <- etaSquared(AnovaStrataov2_xA_14m)
-summary(AnovaCIPLaov2_xA_14m)
-print(EtaCIPL_xA_14m)
-summary(AnovaStrataov2_xA_14m)
-print(EtaStrat_xA_14m)
-
-
-AnovaCIPLaov2_xR_14m <- aov(CIPL_Scores ~ Strain * X_Day, data = TABLExR_14m)
-EtaCIPL_xR_14m <- etaSquared(AnovaCIPLaov2_xR_14m)
-AnovaStrataov2_xR_14m <- aov(strategy ~ Strain * X_Day, data = TABLExR_14m)
-EtaStrat_xR_14m <- etaSquared(AnovaStrataov2_xR_14m)
-summary(AnovaCIPLaov2_xR_14m)
-print(EtaCIPL_xR_14m)
-summary(AnovaStrataov2_xR_14m)
-print(EtaStrat_xA_14m)
-
-
-Means_xA_14m <- aggregate(TABLExA_14m$CIPL_Scores, by = list(TABLExA_14m$Strain), FUN = mean)
-print(Means_xA_14m)
-Means_xR_14m <- aggregate(TABLExR_14m$CIPL_Scores, by = list(TABLExR_14m$Strain), FUN = mean)
-print(Means_xR_14m)
-
-MeansStrat_xA_14m <- aggregate(TABLExA_14m$strategy, by = list(TABLExA_14m$Strain), FUN = mean)
-print(MeansStrat_xA_14m)
-MeansStrat_xR_14m <- aggregate(TABLExR_14m$strategy, by = list(TABLExR_14m$Strain), FUN = mean)
-print(MeansStrat_xR_14m)
-
-
-
-
