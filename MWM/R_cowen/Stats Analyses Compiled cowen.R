03419c972406c2d40cad86e54fb6fef54072e9f5
Stephen Cowen <scowen@email.arizona.edu>
2024-08-27 13:33:10 -0700

    Update Stats Analyses Compiled cowen.R

--- MWM/R_cowen/Stats Analyses Compiled cowen.R
+++ MWM/R_cowen/Stats Analyses Compiled cowen.R
@@ -36,17 +36,17 @@ TABLExF_Full$trial_num[TABLExF_Full$trial_num > 31 & TABLExF_Full$trial_num <= 3
 MWM <- subset(TABLExF_Full, trial_num < 7)
 #MWM <- subset(TABLExF_Full, trial_num > 3 & trial_num < 7) # Limit to just trials 5 and 6 when they know things.
 
-MWM$is_thigmotaxis = (MWM$strategy == 1)*1
-MWM$is_circling    = (MWM$strategy == 2)*1
-MWM$is_random_path = (MWM$strategy == 3)*1
-MWM$is_scanning    = (MWM$strategy == 4)*1
-MWM$is_chaining    = (MWM$strategy == 5)*1
+MWM$is_thigmotaxis  = (MWM$strategy == 1)*1
+MWM$is_circling     = (MWM$strategy == 2)*1
+MWM$is_random_path  = (MWM$strategy == 3)*1
+MWM$is_scanning     = (MWM$strategy == 4)*1
+MWM$is_chaining     = (MWM$strategy == 5)*1
 MWM$is_directed_search = (MWM$strategy == 6)*1
 MWM$is_corrected_path  = (MWM$strategy == 7)*1
 MWM$is_direct_path     = (MWM$strategy == 8)*1 
 MWM$is_perseverance    = (MWM$strategy == 9)*1
 
-TB$perf = TB$mn_dir_path + TB$mn_corrected
+MWM$day_cat2 = MWM$day_cat
 
 ggplot(data = MWM, aes(x = trial_num, CIPL_Scores, color = Strain)) + geom_point() + facet_wrap(~day_cat)
 
@@ -56,11 +56,13 @@ mean(MWM$is_thigmotaxis,na.rm = T)
 mean(MWM$is_directed_search,na.rm = T)
 
 TB <- MWM %>% group_by(day_cat, Strain, animalID, Age.months.) %>% summarize(mn_thig = mean(is_thigmotaxis), mn_circ = mean(is_circling) , mn_rnd = mean(is_random_path) , mn_scan = mean(is_scanning) , mn_chain = mean(is_chaining) , mn_direct = mean(is_directed_search) , mn_corrected = mean(is_corrected_path ), mn_dir_path = mean(is_direct_path), mn_persev = mean(is_perseverance), mn_CIPL = mean(CIPL_Scores) )
+TB$perf = TB$mn_dir_path + TB$mn_corrected
 
 # Strategies from the Konsolaki 2016 paper.
 TB$escape = TB$mn_thig + TB$mn_rnd
-TB$local = TB$mn_scan + TB$mn_chain
+TB$local  = TB$mn_scan + TB$mn_chain
 TB$global = TB$mn_dir_path + TB$mn_corrected # Directed search is not included.
+
 # Other metrics.
 TB$perf = TB$global-TB$escape
 TB$allocentric = TB$mn_dir_path + TB$mn_corrected + TB$mn_direct
@@ -81,6 +83,7 @@ ggplot(data = TB, aes(x =escape, y = mn_CIPL, color = Strain)) + geom_point()+ f
 # Is there a general effect of age?
 ggplot(data = TB, aes(x = factor(Age.months.), y = mn_CIPL, color = Strain)) + geom_violin() + geom_point(position=position_jitterdodge(dodge.width=0.9))  + facet_wrap(~day_cat)
 ggplot(data = TB, aes(x = factor(Age.months.), y = perf, color = Strain)) + geom_violin() + geom_point(position=position_jitterdodge(dodge.width=0.9))  + facet_wrap(~day_cat)
+
 # Same question but use a regression/scatter plot. Should be a correlation.
 ggplot(data = TB, aes(x = Age.months., y = mn_CIPL, color = Strain)) + geom_point() + facet_wrap(~day_cat) +  geom_smooth()
 
@@ -88,6 +91,9 @@ ggplot(data = TB, aes(x = Age.months., y = mn_CIPL, color = Strain)) + geom_poin
 # Statistics
 # install.packages("rstatix")
 # ###################
+
+#pairwise.wilcox.test()
+
 library(rstatix)
 a <- TB[,c('allocentric', 'day_cat', 'Strain', 'animalID','Age.months.' )] %>% group_by(day_cat,Age.months.) %>% kruskal_test(allocentric ~ Strain)
 #b <- TB[,c('allocentric', 'day_cat', 'Strain', 'animalID','Age.months.' )] %>% group_by(day_cat,Age.months.) %>% wilcox.test(allocentric ~ Strain)
